36ce67eeff8d978d0fe05e6b2e7a44e8bdb2bf3f
umutplvn <umutpehlivan2078@gmail.com>
2023-08-31 02:36:58 +0100

    Detail Page

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -21,143 +21,151 @@ const DetailPage = () => {
   const [comment, setComment] = useState(false);
   const { postData, getData } = useDataCall();
   const [open, setOpen] = React.useState(false);
-  const [updateOpen, setUpdateOpen]=useState(false)
+  const [updateOpen, setUpdateOpen] = useState(false);
   const handleClose = () => setOpen(false);
   const handleOpen = () => setOpen(true);
-  const handleUpdateOpen =()=>setUpdateOpen(true)
+  const handleUpdateOpen = () => setUpdateOpen(true);
   const handleUpdateClose = () => setUpdateOpen(false);
 
-  const likedPost= likes.map((item)=>item.post)
-
-
+  const likedPost = likes.map((item) => item.post);
 
   const handleLikes = (id) => {
-    postData("likes", `${id}/`, "")
+    postData("likes", `${id}/`, "");
     getData("blogs");
   };
 
   return (
-
-
-    <Grid container 
-   sx={detailPageStyle}
-    > 
-<Grid item xs={11} sm={10} md={9}  lg={7} xl={5} m={"auto"}>
-<Box
-     
-    >
-      <Box mt={"40px"}>
-        {veri.map((item) => (
-          <Paper
-            key={item.id}
-            elevation={3}
-            sx={{
-              m: "auto",
-              color: "black",
-              backgroundColor: "#eef8fa",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-            }}
-          >
-            <Box height={"200px"} padding={"0.5rem"} textAlign={"center"}>
-              <img
-                src={item.image}
-                width={"250px"}
-                style={{
-                  borderRadius: "1rem",
-                  aspectRatio: "4/3",
-                  objectFit: "contain",
+    <Grid container sx={detailPageStyle}>
+      <Grid item xs={11} sm={10} md={9} lg={7} xl={5} m={"auto"} width={"100%"}>
+        <Box >
+          <Box mt={"40px"}>
+            {veri.map((item) => (
+              <Paper
+                key={item.id}
+                elevation={3}
+                sx={{
+                  m: "auto",
+                  color: "black",
+                  backgroundColor: "#eef8fa",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
                 }}
-              />
-            </Box>
-
-            <Typography
-              component={"h4"}
-              variant="h5"
-              fontWeight={"600"}
-              sx={{ textAlign: "center" }}
-            >
-              {item.title}
-            </Typography>
-
-            {/* Content Text */}
-            <Box padding={"0.5rem"}>
-              <Typography  m={"2rem 0"}>{item.content}</Typography>
-              <Typography sx={{ mt: "15px" }}>{item.publish_date}</Typography>
-            </Box>
-            {/* Content User Info */}
-            <Box
-              display={"flex"}
-              alignItems={"center"}
-              padding={"0.5rem"}
-              gap="10px"
-            >
-              <Avatar>
-                <AccountCircleIcon />
-              </Avatar>
-              <Typography>{item.author}</Typography>
-            </Box>
-
-            {/* Content Buttons and Values Info/ Read More */}
-            <Box
-              display={"flex"}
-              justifyContent={"space-between"}
-              alignItems={"center"}
-            >
-              <Box display={"flex"} padding={"0.5rem"} gap={"0.5rem"}>
-                <Box display={"flex"}>
-                  {likedPost.includes(item.id)  ? (
-                    <FavoriteIcon
-                      sx={{ cursor: "pointer", color: "red" }}
-                      onClick={() => handleLikes(item.id)}
-                    />
-                  ) : (
-                    <FavoriteIcon
-                      sx={{ cursor: "pointer", color: "black" }}
-                      onClick={() => handleLikes(item.id)}
-                    />
-                  )}
-
-                  <Typography>{item.likes}</Typography>
-                </Box>
-                <Box display={"flex"}>
-                  <RemoveRedEyeIcon sx={{ cursor: "pointer" }} />
-                  <Typography>{item.post_views}</Typography>
-                </Box>
-                <Box display={"flex"}>
-                  <ChatBubbleIcon
-                    onClick={() => setComment(!comment)}
-                    sx={{ cursor: "pointer" }}
+              >
+                <Box height={"200px"} padding={"0.5rem"} textAlign={"center"}>
+                  <img
+                    src={item.image}
+                    width={"250px"}
+                    style={{
+                      borderRadius: "1rem",
+                      aspectRatio: "4/3",
+                      objectFit: "contain",
+                    }}
                   />
-                  <Typography>{item.comment_count}</Typography>
                 </Box>
-              </Box>
-            </Box>
-             { item.author == currentUser &&  
-             <>
-             <Box display={"flex"}  justifyContent={"center"} gap={2} m="1rem">
-              <Button  sx={btnRed} onClick={handleOpen}>Delete</Button>
-              <Button sx={btnGreen} onClick={handleUpdateOpen}>Update</Button>
-             </Box>
-             
-             <DeleteModal open={open} handleClose={handleClose} handleOpen={handleOpen} id={item.id}/>
-             </>
-             }
+
+                <Typography
+                  component={"h4"}
+                  variant="h5"
+                  fontWeight={"600"}
+                  sx={{ textAlign: "center" }}
+                >
+                  {item.title}
+                </Typography>
+
+                {/* Content Text */}
+                <Box padding={"0.5rem"}>
+                  <Typography m={"2rem 0"}>{item.content}</Typography>
+                  <Typography sx={{ mt: "15px" }}>
+                    {item.publish_date}
+                  </Typography>
+                </Box>
+                {/* Content User Info */}
+                <Box
+                  display={"flex"}
+                  alignItems={"center"}
+                  padding={"0.5rem"}
+                  gap="10px"
+                >
+                  <Avatar>
+                    <AccountCircleIcon />
+                  </Avatar>
+                  <Typography>{item.author}</Typography>
+                </Box>
+
+                {/* Content Buttons and Values Info/ Read More */}
+                <Box
+                  display={"flex"}
+                  justifyContent={"space-between"}
+                  alignItems={"center"}
+                >
+                  <Box display={"flex"} padding={"0.5rem"} gap={"0.5rem"}>
+                    <Box display={"flex"}>
+                      {likedPost.includes(item.id) ? (
+                        <FavoriteIcon
+                          sx={{ cursor: "pointer", color: "red" }}
+                          onClick={() => handleLikes(item.id)}
+                        />
+                      ) : (
+                        <FavoriteIcon
+                          sx={{ cursor: "pointer", color: "black" }}
+                          onClick={() => handleLikes(item.id)}
+                        />
+                      )}
+
+                      <Typography>{item.likes}</Typography>
+                    </Box>
+                    <Box display={"flex"}>
+                      <RemoveRedEyeIcon sx={{ cursor: "pointer" }} />
+                      <Typography>{item.post_views}</Typography>
+                    </Box>
+                    <Box display={"flex"}>
+                      <ChatBubbleIcon
+                        onClick={() => setComment(!comment)}
+                        sx={{ cursor: "pointer" }}
+                      />
+                      <Typography>{item.comment_count}</Typography>
+                    </Box>
+                  </Box>
+                </Box>
+                {item.author == currentUser && (
+                  <>
+                    <Box
+                      display={"flex"}
+                      justifyContent={"center"}
+                      gap={2}
+                      m="1rem"
+                    >
+                      <Button sx={btnRed} onClick={handleOpen}>
+                        Delete
+                      </Button>
+                      <Button sx={btnGreen} onClick={handleUpdateOpen}>
+                        Update
+                      </Button>
+                    </Box>
+
+                    <DeleteModal
+                      open={open}
+                      handleClose={handleClose}
+                      handleOpen={handleOpen}
+                      id={item.id}
+                    />
+                  </>
+                )}
+
+                {comment && <CommentBlock id={item.id} />}
+                <UpdateModal
+                  updateOpen={updateOpen}
+                  handleUpdateClose={handleUpdateClose}
+                  item={item}
+                />
+              </Paper>
+            ))}
+          </Box>
+        </Box>
         
-
-            {comment && <CommentBlock id={item.id}/>}
-            <UpdateModal updateOpen={updateOpen} handleUpdateClose={handleUpdateClose} item={item}/>
-          </Paper>
-        ))}
-      </Box>
-    </Box>
-</Grid>
-
+      </Grid>
     </Grid>
-
-
-   
   );
 };
 export default DetailPage;
